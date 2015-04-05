2894842e0c24961205fd7f5f4db19673c9569cf9
Dan Nguyen <pedantic@gmail.com>
2015-04-04 19:10:50 -0500

    Rename stuff

--- haskell/14/Problem14.hs
+++ haskell/14/Problem14.hs
@@ -1,21 +1,27 @@
 import Data.List (maximumBy,foldl1')
 import Data.Ord (comparing)
 
-s :: Integer -> [Integer]
-s n | n == 1         = [1]
-    | n `mod` 2 == 0 = n : s (n `div` 2)
-    | otherwise      = n : s (3 * n + 1)
+collatz :: Integer -> [Integer]
+collatz n | n == 1    = [1]
+          | even n    = n : collatz (n `div` 2)
+          | otherwise = n : collatz (3 * n + 1)
 
-seqLen = length . s
+collatzLen = length . collatz
 
-lens = map (\x -> (x, seqLen x)) [2..1000000]
+lengths = map (\x -> (x, collatzLen x)) [2..1000000]
 
--- This will stack overflow when compiled without optimization
--- alternatively, we can use foldl1' to avoid thunking
--- real    0m4.880s
--- user    0m1.160s
--- sys     0m3.636s
-problem14 = foldl1' longest lens
+{- Using foldl1 will stack overflow when compiled without optimization
+   alternatively, we can use foldl1' to avoid thunking
+   real    0m4.880s
+   user    0m1.160s
+   sys     0m3.636s
+
+   In ghci, this isn't a problem.
+   *Main> problem14
+   837799
+   (55.90s, 53065318736 bytes)
+-}
+problem14 = fst $ foldl1' longest lengths
   where longest a@(_,a2) b@(_,b2) | a2 >= b2  = a
                                   | otherwise = b
 
@@ -23,6 +29,6 @@ problem14 = foldl1' longest lens
 -- real    0m20.952s
 -- user    0m5.976s
 -- sys     0m14.872s
-problem14a = maximumBy (comparing seqLen) [2..1000000]
+problem14a = maximumBy (comparing collatzLen) [2..1000000]
 
 main = print problem14
