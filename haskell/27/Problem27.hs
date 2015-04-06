3a5cbcb72ad0200c01e620cc483ea014d046609d
Dan Nguyen <pedantic@gmail.com>
2015-04-06 01:19:31 -0500

    improve performance

--- haskell/27/Problem27.hs
+++ haskell/27/Problem27.hs
@@ -9,17 +9,23 @@ primes = 2:3:mapMaybe (next (tail primes)) [5,7..]
           | n `rem` x == 0 = Nothing
           | otherwise      = next xs n
 
+{- Using elem won't work because primes is infinite
+   But since primes is in order, we can drop all numbers
+   less than n, and compare if the first number remaining
+   is the same.
+-}
 isPrime n = n == (head . dropWhile (<n) $ primes)
 
-testConsPrime f = tcp' 0
+testConsecutivePrime f = tcp' 0
   where tcp' n | isPrime (f n) = tcp' (n + 1)
                | otherwise     = n
 
 q a b n = n * n + a*n + b
 
-combinations = [ (testConsPrime (q a b), a*b)
+-- b must be prime, because for q a b 0 = b
+combinations = [ (testConsecutivePrime (q a b), a*b)
                | a <- [-1000..1000]
-               , b <- [-1000..1000]
+               , b <- takeWhile (<1000) primes
                ]
 
 filterFst t = filter (t . fst)
