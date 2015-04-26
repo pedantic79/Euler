dc5126cecbb7a57f147acedb6ea96c70114b7e12
Dan Nguyen <pedantic@gmail.com>
2015-04-26 00:38:05 -0500

    remove trailing whitespace

--- haskell/35/Problem35.hs
+++ haskell/35/Problem35.hs
@@ -2,17 +2,17 @@ import Data.List (inits)
 import Data.Maybe (mapMaybe)
 import qualified Data.Set as Set
 
--- primeST from haskell wiki
-primes = 2 : oddprimes
+-- from haskell wiki
+primesST = 2 : oddprimes
   where
     oddprimes = sieve 3 9 oddprimes (inits oddprimes)  -- [],[3],[3,5],...
     sieve x q ~(_:t) (fs:ft) =
       filter ((`all` fs) . ((/=0).) . rem) [x,x+2..q-2]
       ++ sieve (q+2) (head t^2) t ft
-      
+
 rotateNum n = helper n
   where
-    (num,pow10) = numLen n (1,1)    
+    (num,pow10) = numLen n (1,1)
     helper m = take num $ nextNum:helper nextNum
       where nextNum = q + r
             (q,r) = (m * 10) `divMod` pow10
@@ -27,5 +27,5 @@ check s n
 
 problem35 = succ . length $ mapMaybe (check primeSet) [3,5..limit]
   where
-    primeSet = Set.fromList $ takeWhile (<limit) primes    
+    primeSet = Set.fromList $ takeWhile (<limit) primesST
     limit = 1000000
