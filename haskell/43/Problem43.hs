af77f2e3e61e6e0f08bd32c9bfa2404f83c1fc2f
Dan Nguyen <pedantic@gmail.com>
2015-05-03 17:31:48 -0500

    Use foldl

--- haskell/43/Problem43.hs
+++ haskell/43/Problem43.hs
@@ -1,8 +1,8 @@
-import Data.List (permutations,(\\))
+import Data.List ((\\))
 
 num = "1406357289"
 primes = [2,3,5,7,11,13,17] :: [Int]
-factors = [17,7,2,1]
+factors = [17,7,2,0]
 
 str2Int :: String -> Integer
 str2Int = read
@@ -11,16 +11,17 @@ isSpecial = all ((==0) . uncurry mod) . flip zip primes . subNumbers
   where
     subNumbers n = map (read . ($n) . (take 3 .) . drop) [1..7]
 
-genSubStr ns = [a:b:[c] | a <- ns, b <- ns, c <-ns, a/=b && b/=c && c/= a]
+-- 3-length permutations
+perm3 ns = [a:b:[c] | a <- ns, b <- ns, c <- ns, a/=b && b/=c && c/= a]
 
 -- generate all 3 digit numbers from the third parameter
 -- only include those who are divisible by p
 -- Then append app to to that.
--- if p is 1, then we just append. This is the case where
+-- if p is 0, then we just append. This is the case where
 -- the last digit needs to be found
-fDivisible p app 
-  | p == 1    = flip (:) [] . (++app) -- [param3 ++ app]
-  | otherwise = map (++app) . filter isMod0 . genSubStr
+fMod p app -- param3
+  | p == 0    = flip (:) [] . (++app) -- [param3 ++ app]
+  | otherwise = map (++app) . filter isMod0 . perm3
   where
     isMod0 = (==0) . flip mod p . str2Int
 
@@ -28,17 +29,9 @@ fDivisible p app
 -- by 2, digits_567 divisible by 7, and digits_890 by 17. digit_1 is
 -- the missing digit.
 --
--- We go right to left, with factors passed to (p:ps) and [""] as our
--- initial value. This will generate digits_890 that are divisible by
--- 17. Then we loop with the list coming from p=17 for p=7.
--- For p=7, the we map over each acc value, and use fDivisible to
--- generate all digits divisible by 7 that are coming from the set
--- that do not include the acc value being mapped.
--- 
--- This generates relatively few candidates.
-gen []     acc = acc
-gen (p:ps) acc = gen ps nAcc
-  where nAcc = concatMap (\x -> fDivisible p x (num \\ x)) acc
+-- This generates relatively few candidates (16182 vs 3628800)
+gen = foldl (flip cMap) [""]
+  where
+    cMap p = concatMap (\x -> fMod p x (num \\ x))
 
-problem43 = sum . map str2Int . filter isSpecial . gen factors $ [""]
-
+problem43 = sum . map str2Int . filter isSpecial . gen $ factors
