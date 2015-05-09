bf3e541b4690166a876fa4e7aabd9b2f1784a677
Dan Nguyen <pedantic@gmail.com>
2015-05-09 18:01:23 -0500

    Add 46

---
+++ haskell/46/Program46.hs
@@ -0,0 +1,10 @@
+import Data.Numbers.Primes
+
+isGoldbach n = or [n == p + 2*s^2 | p <- ps, let s = fn (n - p)]
+  where
+    ps = takeWhile (<n) primes
+    fn = floor . sqrt . fromIntegral . flip div 2
+
+filterNot = filter . (not .)
+
+problem46 = head . filterNot isGoldbach . filterNot isPrime $ [27,29..]
