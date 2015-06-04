2a953f86f51a6fab8567d330b0a99175f4c71efe
Dan Nguyen <pedantic@gmail.com>
2015-06-03 22:15:38 -0500

    more efficient version

--- haskell/51/Problem51.hs
+++ haskell/51/Problem51.hs
@@ -1,19 +1,22 @@
 import Data.Numbers.Primes
+import Data.List (elemIndices)
 
 str2Int :: String -> Int
 str2Int = read
 
 primeL p = dropWhile (<10^p) . takeWhile (<2*10^p) $ primes
-
 strReplace fm to = map (\c -> if c == fm then to else c) 
-
 variations fm str = map (str2Int . flip (strReplace fm) str) "0123456789"
-
 primeVars p ch = filter isPrime . filter (>10^p) . variations ch
-
 run p ch = filter ((==8) . length) . map (primeVars p ch . show) $ primeL p
-
 problem51 = head . concat $ run 5 '1'
 
-main = print . head . concat . concat $
-       [run n ch | n <- [5], ch <- "1023456789" ]
+
+f = head $ [ str2Int p | p <- map show primes, any (check p) "0123456789" ]
+repStr fr to str = read $ map (\c -> if c == fr then to else c) str
+
+check p d = length idxs >= 3 && length vartns == 8
+  where idxs = elemIndices d p
+        vartns = [ r | r <- "0123456789"
+                     , isPrime (repStr d r p)
+                     , r /= '0' || (0 `notElem` idxs)]
