7e9f7296ffd587e9cd34c1012c395f12817b3e31
Dan Nguyen <pedantic@gmail.com>
2015-08-20 23:40:50 -0500

    Add 58

---
+++ haskell/58/Problem58.hs
@@ -0,0 +1,44 @@
+import Data.List (inits,findIndex)
+import Data.Maybe (fromJust)
+import Data.Numbers.Primes (isPrime)
+
+-- Taken from problem 28
+rowStp = map (\x -> ((x - 2)^2+1, x^2, x-2)) [3,5..]
+
+integerSqrt = toInteger . floor . sqrt . fromIntegral
+
+-- Produce the corner values
+corners = map mkRow rowStp
+  where
+    mkRow (l,h,s) = let a = l + s
+                        b = a + s + 1
+                    in [a, b .. h]
+      
+primeCorners = map (toInteger . length . filter isPrime) corners
+count = let num = 5 : repeat 4
+        in zip primeCorners num
+
+intPerc x y = x * 100 `div` y
+
+percentage = uncurry intPerc . sums
+  where sums = foldr1 (\(a,b) (c,d) -> (a+c, b+d))
+
+f = fromJust . findIndex (<10) . map percentage . tail . inits $ count
+
+-- 26241
+-- 47.09s/14.880s
+problem58a = integerSqrt . last $ corners !! f
+
+percentMap = pMap 0 0
+  where
+    pMap pCnt tot ((p,q):ps) =
+      let pCnt' = p + pCnt
+          tot' = q + tot
+      in intPerc pCnt' tot' : pMap pCnt' tot' ps
+
+--  9.98s/0.439s
+problem58 = integerSqrt . last $ corners !! l
+  where l = fromJust . findIndex (<10) . percentMap $ count
+
+main = print problem58
+        
