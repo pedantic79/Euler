a89a26ac4bda47aaf1e1608bdad28d2c85065557
Dan Nguyen <pedantic@gmail.com>
2015-09-18 11:43:11 -0500

    Working version of 61

--- haskell/61/Problem61.hs
+++ haskell/61/Problem61.hs
@@ -1,5 +1,4 @@
-import qualified Data.Set as S
-import Data.List (foldl')
+import qualified Data.List as L
 import qualified Data.Array as A
 
 figurative x n
@@ -8,46 +7,44 @@ figurative x n
   | x == 5 = (3*n*n - n) `div` 2
   | x == 6 =  2*n*n - n
   | x == 7 = (5*n*n - 3*n) `div` 2
-  | x == 8 = (3*n*n - 2*n)
+  | x == 8 =  3*n*n - 2*n
 
+figValid x
+  | x == 3 = [45..99] ++ [102..140]
+  | x == 4 = [32..99]
+  | x == 5 = [26..33] ++ [35..81]
+  | x == 6 = [23..50] ++ [52..70]
+  | x == 7 = [21..40] ++ [42..63]
+  | x == 8 = [19..58]
+
+fig4Dig x = map (\n -> (x, figurative x n)) $ figValid x
+
+front = (`div` 100)
 back = (`mod` 100)
 
-fig4 = takeWhile ((<10000).snd) . dropWhile ((<999).snd) . figTup
+figData = A.listArray (10,99) v
   where
-    figTup x = map (\n -> (n, figurative x n)) [1..]
-    filt = filter $ \(_,n) -> back n > 9
-    
+    v = L.groupBy g . L.sortBy s . concatMap fig4Dig $ [3..8]
+      where
+        g (_,a) (_,b) = front a == front b
+        s (_,a) (_,b) = compare a b
 
-fig4Array x = A.listArray (i, i+l-1) d
-  where
-    v = fig4 x
-    i = fst . head $ v
-    d = map snd v
-    l = length d
+-- figData = M.fromList $ map (\l -> (front . snd $ head l, l)) v
 
-filt rs = filter (not . (`elem` rs) . fst)
+getNext x rs
+  | x < 10 = []
+  | otherwise = filter (\(n,_) -> notElem n rs) $ figData A.! x
 
-maxVal = 6
-
-figArray = A.listArray (3, maxVal) (map fig4Array [3..maxVal])
-
-getSet x
-  | x == maxVal = S.fromList $ dig x
-  | otherwise = foldl' (flip S.insert) (getSet (x+1)) $ dig x
-  where
-    dig = map ((`div` 100) . snd) . fig4
-
-getSetArray = A.listArray (3,maxVal) (map getSet [3..maxVal])
-
-f n rs
-  | n == maxVal = map (:[]) . filt rs . A.assocs $ figArray A.! n
-  | otherwise = [ num:more | num <-  A.assocs $ figArray A.! n
-                           , more <- f (n+1) (fst num:rs)
-                           , not $ elem (snd num) (map snd more)
-                           , S.member (start num) $ getSetArray A.! n
+q f x rs
+  | null next = [[f]]
+  | otherwise = [ val:more | (num,val) <- next
+                           , more <- q f (back val) (num:rs)
                            ]
-  where
-    start = (`mod` 100) . snd
+  where next = getNext x rs
 
-problem61 = length $ f 3 []
-main = print problem61
+-- 28684
+problem61 = sum . head . filter filt .
+            filter (\l -> length l == 6) .
+            concatMap (\(n,v) -> q v (back v) [n]) $ fig4Dig 3
+  where
+    filt l = (front . last $ l) == (back . last . init $ l)
