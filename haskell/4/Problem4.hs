3a370b33ee15dec602337b44af5880bae688987a
Dan Nguyen <pedantic@gmail.com>
2015-10-25 15:51:51 -0500

    Add hspec test for problem 4

--- haskell/4/Problem4.hs
+++ haskell/4/Problem4.hs
@@ -1,2 +1,17 @@
-products = [a * b | a <- [100..999], b <- [100..999]]
-problem4 = maximum $ filter (\x -> show x == reverse (show x)) products
+import Test.Hspec
+
+products l = [a * b | a <- s, b <- s]
+  where s = [10^(l-1) .. 10^l-1]
+
+maxPalin = maximum . filter (\x -> isPalin (show x))
+  where isPalin xs = xs == reverse xs
+
+problem4 = maxPalin $ products 3
+
+main = hspec $ 
+  describe "problem4" $ do
+    it "Largest palindrome made from the product of two 2-digit numbers" $
+      maxPalin (products 2) `shouldBe` 9009
+  
+    it "Largest palindrome made from the product of two 3-digit numbers" $
+      problem4 `shouldBe` 906609
