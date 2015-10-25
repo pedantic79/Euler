155e3184c86356eeb3c4157c3bc4b87ebfa4879d
Dan Nguyen <pedantic@gmail.com>
2015-10-25 14:55:23 -0500

    Add hspec test for problem 2

--- haskell/2/Problem2.hs
+++ haskell/2/Problem2.hs
@@ -1,8 +1,15 @@
+import Test.Hspec
+
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
 fibsIterate = map fst . iterate (\(a,b) -> (b,a+b)) $ (1,2)
 
 problem2 = sum [ x | x <- takeWhile (< 4000000) fibs, even x]
 
-
-
+main = hspec $ 
+  describe "problem2" $ do
+    it "First 10 fibonacci terms" $
+     take 10 fibs `shouldBe` [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  
+    it "Sum of even fibonacci numbers that are less than 4 million" $
+      problem2 `shouldBe` 4613732
