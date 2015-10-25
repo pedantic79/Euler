81d5f34a51b547bd05f4d973712f77ee805a0754
Dan Nguyen <pedantic@gmail.com>
2015-10-25 14:53:57 -0500

    Add hspec test for problem 1

--- haskell/1/Problem1.hs
+++ haskell/1/Problem1.hs
@@ -1,1 +1,14 @@
-problem1 = sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+import Test.Hspec
+
+mult3or5 n = sum [ x | x <- [1..(n-1)]
+                     , x `mod` 3 == 0 || x `mod` 5 == 0 ]
+
+problem1 = mult3or5 1000
+
+main  = hspec $
+  describe "problem1" $ do
+    it "Sum of all multiples of 3 or 5 below 10" $
+      mult3or5 10 `shouldBe` 23
+
+    it "Sum of all the multiples of 3 or 5 below 1000" $
+      problem1 `shouldBe` 233168
