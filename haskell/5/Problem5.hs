721ceb2be5f9cf19d7655d76193c4148a84f0b84
Dan Nguyen <pedantic@gmail.com>
2015-10-25 22:30:07 -0500

    Add hspec test for problem 5

--- haskell/5/Problem5.hs
+++ haskell/5/Problem5.hs
@@ -1,7 +1,20 @@
+import Test.Hspec
+
 remainder n = all (\x -> n `mod` x == 0) [2..20]
 
+-- 380 = 19 * 20
 problem5a = helper 380
   where helper n | remainder n = n
                  | otherwise   = helper (n + 380)
 
-problem5 = foldl1 lcm [1..20]                                 
+multiples n = foldl1 lcm [1..n]
+
+problem5 = multiples 20
+
+main = hspec $
+  describe "problem5" $ do
+    it "Smallest number divisible by all numbers 1 to 10" $
+      multiples 10 `shouldBe` 2520
+
+    it "Smallest number divisible by all numbers 1 to 20" $
+      problem5 `shouldBe` 232792560
