26a62f87747f846ce3870656409797d185c7eb53
Dan Nguyen <pedantic@gmail.com>
2015-10-25 22:34:33 -0500

    Add hspec test for problem 6

--- haskell/6/Problem6.hs
+++ haskell/6/Problem6.hs
@@ -1,4 +1,16 @@
+import Test.Hspec
+
 sumOfSquares = sum . map (^2)
 squareOfSums = (^2) . sum
 
-problem6 = squareOfSums [1..100] - sumOfSquares [1..100]
+diff n = squareOfSums [1..n] - sumOfSquares [1..n]
+
+problem6 = diff 100
+
+main = hspec $
+  describe "problem6" $ do
+    it "Square of the sums of 1 to 10 - sum of squares of 1 to 10" $
+      diff 10 `shouldBe` 2640
+
+    it "Square of the sums of 1 to 100 - sum of squares of 1 to 100" $
+      problem6 `shouldBe` 25164150
